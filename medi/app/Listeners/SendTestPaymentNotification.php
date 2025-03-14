affaf2d53f6f7090609e11affcd163d9b8d381ba
yonas-jemberu-kebede <jemberuyonas01@gmail.com>
2025-03-14 17:39:38 +0300

    email notificcation defined

---
+++ medi/app/Listeners/SendTestPaymentNotification.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\TestPaymentRequested;
+use App\Mail\TestPaymentRequestEmail;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Mail;
+class SendTestPaymentNotification
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct(TestPaymentRequested $event)
+    {
+
+
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(TestPaymentRequested $event): void
+    {
+        $pendingTesting = $event->pendingTesting;
+        $patient = $pendingTesting->patient;
+        $payment = $pendingTesting->payment;
+
+        Mail::to($patient->email)->send(new TestPaymentRequestEmail($pendingTesting,$payment));
+    }
+}
