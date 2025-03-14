7ec12bb69c9524e2db7a0621dd53c8904a0fecb7
yonas-jemberu-kebede <jemberuyonas01@gmail.com>
2025-03-14 17:41:00 +0300

    name modfication

---
+++ medi/app/Models/PendingTesting.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PendingTesting extends Model
+{
+    /** @use HasFactory<\Database\Factories\PendingTesingFactory> */
+    use HasFactory;
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'test_requests' => 'array',
+    ];
+
+    // Relationships
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function hospital()
+    {
+        return $this->belongsTo(Hospital::class);
+    }
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+
+    public function labTechnician()
+    {
+        return $this->belongsTo(LabTechnician::class);
+    }
+
+    public function payment()
+    {
+        return $this->morphOne(Payment::class, 'payable');
+    }
+}
