6cbd84b0c1030cbb7d0e689537239d80cb37c773
yonas-jemberu-kebede <jemberuyonas01@gmail.com>
2025-03-28 17:27:48 +0300

    defining pharmacist table

--- medi/database/migrations/2025_02_17_080400_create_pharmacists_table.php
+++ medi/database/migrations/2025_02_17_080400_create_pharmacists_table.php
@@ -22,6 +22,9 @@ return new class extends Migration
             $table->enum('gender', ['Male', 'Female']);
             $table->string('phone_number');
             $table->date('date_of_birth');
+            $table->string('shift_day');
+            $table->time('shift_start');
+            $table->time('shift_end');
             $table->timestamps();
 
             $table->foreign('pharmacy_id')->references('id')->on('pharmacies')->onDelete('cascade');
