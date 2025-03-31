99489baea7717a1686eb286c9bbda37eb75d090e
yonas-jemberu-kebede <jemberuyonas01@gmail.com>
2025-03-31 16:15:07 +0300

    pending prescription created to store the prescription data temporarirly

---
+++ medi/database/migrations/2025_03_28_163031_create_pending_prescriptions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pending_prescriptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('patient_id')->constrained('patients')->cascadeOnDelete();
+            $table->foreignId('doctor_id')->constrained('doctors')->cascadeOnDelete();
+            $table->foreignId('test_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->foreignId('hospital_id')->constrained('hospitals')->cascadeOnDelete();
+            $table->foreignId('pharmacy_id')->constrained('pharmacies')->onDelete('cascade');
+            $table->foreignId('pharmacist_id')->nullable()->constrained()->onDelete('cascade');
+
+            $table->string('status')->default('pending');
+            $table->json('medications'); // e.g., [{"name": "Aspirin", "dosage": "500mg", "frequency": "daily"}]
+            $table->text('instructions')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pending_prescriptions');
+    }
+};
