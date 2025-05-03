a4d45e99bf44f8707174340daad5b06b4ef697bf
yonas-jemberu-kebede <jemberuyonas01@gmail.com>
2025-05-04 01:45:43 +0300

    modification on pending_testings table

---
+++ medi/database/migrations/2025_05_03_202529_create_pending_testings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pending_testings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('patient_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('doctor_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('lab_technician_id')->nullable()->constrained()->cascadeOnDelete();
+
+            $table->json('test_requests');
+            $table->decimal('total_amount', 8, 2);
+            $table->string('status')->default('pending_payment');
+            $table->string('tx_ref')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pending_testings');
+    }
+};
